e6fe038503f410b92ada321e23984ce4291023d9
arfat132 <arfatbegum90@gmail.com>
2023-11-05 12:00:28 +0300

    Update admin feedback

--- src/app/(withlayout)/admin/feedback/page.tsx
+++ src/app/(withlayout)/admin/feedback/page.tsx
@@ -1,28 +1,60 @@
 "use client";
 
-import Link from "next/link";
 import {
     ReloadOutlined,
+    SearchOutlined
 } from "@ant-design/icons";
 import dayjs from "dayjs";
 import BreadCrumb from "@/components/UI/Shared/BreadCrumb";
 import ActionBar from "@/components/UI/Shared/ActionBar";
 import DataTable from "@/components/UI/Shared/DataTable";
 import { useFeedbacksQuery } from "@/redux/api/FeedbackApi";
+import { useState } from "react";
+import { useDebounced } from "@/redux/hooks";
+import { Input } from "antd";
 
 const Feedbacks = () => {
     const query: Record<string, any> = {};
 
+    const [page, setPage] = useState<number>(1);
+    const [size, setSize] = useState<number>(10);
+    const [sortBy, setSortBy] = useState<string>("");
+    const [sortOrder, setSortOrder] = useState<string>("");
+    const [searchTerm, setSearchTerm] = useState<string>("");
+
+    query["limit"] = size;
+    query["page"] = page;
+    query["sortBy"] = sortBy;
+    query["sortOrder"] = sortOrder;
+
+    const debouncedSearchTerm = useDebounced({
+        searchQuery: searchTerm,
+        delay: 600,
+    });
+
+    if (!!debouncedSearchTerm) {
+        query["searchTerm"] = debouncedSearchTerm;
+    }
+
     const { data, isLoading } = useFeedbacksQuery({ ...query });
     const feedbacks = data?.feedbacks;
+    const meta = data?.meta;
 
     const columns = [
         {
             title: "Name",
             dataIndex: "user",
-            render: function (data: Record<string, string>) {
+            render: function (data: any) {
                 const fullName = `${data?.name}`;
                 return <>{fullName}</>;
+            },
+        },
+        {
+            title: "Email",
+            dataIndex: "user",
+            render: function (data: any) {
+                const email = `${data?.email}`;
+                return <>{email}</>;
             },
         },
         {
@@ -43,6 +75,22 @@ const Feedbacks = () => {
         },
     ];
 
+    const onPaginationChange = (page: number, pageSize: number) => {
+        setPage(page);
+        setSize(pageSize);
+    };
+    const onTableChange = (pagination: any, filter: any, sorter: any) => {
+        const { order, field } = sorter;
+        setSortBy(field as string);
+        setSortOrder(order === "ascend" ? "asc" : "desc");
+    };
+
+    const resetFilters = () => {
+        setSortBy("");
+        setSortOrder("");
+        setSearchTerm("");
+    };
+
     return (
         <div>
             <BreadCrumb
@@ -53,11 +101,33 @@ const Feedbacks = () => {
                     },
                 ]}
             />
-            <ActionBar title="Feedback List" />
+            <ActionBar title="Feedback List" >
+                <Input
+                    addonBefore={<SearchOutlined style={{ fontSize: '18px', color: "#4338ca" }} />}
+                    placeholder="Search ......"
+                    onChange={(e) => {
+                        setSearchTerm(e.target.value);
+                    }}
+                />
+                {(!!sortBy || !!sortOrder || !!searchTerm) && (
+                    <button
+                        onClick={resetFilters}
+                        className="bg-[#0f337a] px-4 py-2 ml-2 text-white rounded font-semibold float-right"
+                    >
+                        <ReloadOutlined />
+                    </button>
+                )}
+            </ActionBar>
             <DataTable
                 loading={isLoading}
                 columns={columns}
                 dataSource={feedbacks}
+                pageSize={size}
+                totalPages={meta?.total}
+                showSizeChanger={true}
+                onPaginationChange={onPaginationChange}
+                onTableChange={onTableChange}
+                showPagination={true}
             />
         </div>
     );
